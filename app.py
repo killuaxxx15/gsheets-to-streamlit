c25ef5e2330775d4856658155105808f87656c32
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-01-09 14:34:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ st.set_page_config(page_title='Sharks Data Collection')
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
+# Read data from google sheets
 df = conn.read(
   worksheet="Sheet1",
   ttl="5"
 )
+
+# Preprocess DataFrame: Remove empty rows/columns or replace empty cells
+df.dropna(how='all', inplace=True)  # Removes rows where all cells are empty
+df.dropna(axis=1, how='all', inplace=True)  # Uncomment to remove columns where all cells are empty
+# df.fillna('Your Placeholder', inplace=True)  # Uncomment to replace empty cells with a placeholder
 
 # Display content
 st.markdown(' # Sharks Data Collection ')
