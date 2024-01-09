55c123ed1949147b2a0de44e9a743175df91a659
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-01-09 14:36:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,16 @@ df.dropna(axis=1, how='all', inplace=True)  # Uncomment to remove columns where 
 
 # Display content
 st.markdown(' # Sharks Data Collection ')
-st.dataframe(df)
+
+st.sidebar.header("Please filter here:")
+shark_name = st.sidebar.multiselect(
+    "Select the Shark Name:",
+    options = df['Shark_Name'].unique(),
+    default = df['Shark_Name'].unique()
+)
+
+df_selection = df.query(
+    "Shark_Name == @shark_name"
+)
+
+st.dataframe(df_selection)
